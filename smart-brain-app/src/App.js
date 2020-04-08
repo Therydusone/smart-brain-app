12d49cb41d1f6eb2ec2a60461a95551d87eb6f0c
Therydusone <przem.pajak@gmail.com>
2020-04-08 19:10:54 +0200

    added particles

--- smart-brain-app/src/App.js
+++ smart-brain-app/src/App.js
@@ -3,13 +3,38 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 
+const particlesOptions = {
+    particles: {
+      number: { 
+        value: 200, 
+        density: { 
+          enable: true, 
+          value_area: 1000, 
+        }, 
+      line_linked: {
+        shadow: {
+          enable: true,
+          color: "#3CA9D1",
+          blur: 5
+        }
+      }
+    }
+  }
+}
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
+    <Particles 
+      className="particles" 
+      params={particlesOptions}
+    />
+    <Navigation />
+    <Logo />
+    <Rank />
+    <ImageLinkForm />
     </div>
   );
 }
